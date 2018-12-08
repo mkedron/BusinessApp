646a815eae2b6d660fd752f49951f1590d397449
mkedron <mkkedron@gmail.com>
2018-12-08 11:10:40 +0100

    Initial commit

---
+++ app/src/main/java/pl/edu/vistula/mkedron/businessapp/BusinessAppMA.kt
@@ -0,0 +1,12 @@
+package pl.edu.vistula.mkedron.businessapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BusinessAppMA : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_business_app_m)
+    }
+}
